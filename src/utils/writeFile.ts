d6d21a0de20bae4eef5a390af8521e6ae2c2cc56
zarif-al <zarif_al96@outlook.com>
2022-04-18 12:27:36 +0600

    bud fix

--- src/utils/writeFile.ts
+++ src/utils/writeFile.ts
@@ -1,14 +1,28 @@
-import { writeFileSync } from 'fs';
+import { writeFileSync, existsSync, mkdirSync } from 'fs';
+import { dirname } from 'path';
 import { unparse } from 'papaparse';
 import {
   customerInputStub,
   customerDirtyInputStub,
 } from '@/entities/customers/test/stubs/customer.stub';
 
+function ensureDirectoryExistence(path) {
+  const dir = dirname(path);
+
+  if (existsSync(dir)) {
+    return true;
+  }
+
+  ensureDirectoryExistence(dir);
+
+  mkdirSync(dir);
+}
+
 export function writeFile(
   path: string,
   option: 'clean' | 'dirty' = 'clean',
 ): void {
+  ensureDirectoryExistence(path);
   let csv;
   if (option == 'clean') {
     csv = unparse([customerInputStub()]);
